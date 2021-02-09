c0c8b947b1d68528e7ef4505c431fc9ba8d46d74
Ubuntu <ubuntu@ip-172-31-15-219.ec2.internal>
2021-02-09 02:08:43 +0000

    completed test

--- index.js
+++ index.js
@@ -85,37 +85,84 @@ let dogs = [
         "weight": 8,
         "votes": 141
     }
-]
+];
 
 // 1.
-var greeting;
-
+var greeting = function(greeting, location, time) {
+    console.log(greeting + " " + location + " " + time);
+};
 
 
 // 2.
-var contestants;
-
+var contestants = function(array) {
+    return array.length;
+};
 
 
 // 3.
-var filterSpecies;
-
-
+var filterSpecies = dogs.filter(function(animalObj, index, array) {
+    console.log(animalObj);
+    return animalObj.species === "dog";
+});
 
 // 4. 
-var dogContestants;
-
+var dogContestants = [...filterSpecies];
 
 
 // 5. 
-var dogsWithClasses;
-    
-
+var dogsWithClasses = dogContestants.map(function(dog, index, array) {
+    if (dog.weight <= 10) {
+        dog.class = "red";
+        return dog;
+    } else if (dog.weight >= 11 && dog.weight <= 20) {
+        dog.class = "yellow";
+        return dog;
+    } else {
+        dog.class = "green";
+        return dog;
+    }
+});
 
 // 6.
-var firstInClass;
+var firstInClass = function(array) {
+    if (array.length === 0) {
+        return {};
+    } else {
+        // return Object.assign( array[0] , firstInClass( array.slice(1) ) );
+        return {...array[0], ...firstInClass( array.slice(1) )};
+    }
+};
+
+// Number six answer should look like this
+/*
+{
+    redAge: 26,
+    redClass: "red",
+    redName: "Higgs",
+    redSpecies: "dog",
+    redVotes: 200,
+    redWeight: 0.00001,
+    yellowAge: 12,
+    yellowClass: "yellow",
+    yellowName: "Lepton",
+    yellowSpecies: "dog",
+    yellowVotes: 175,
+    yellowWeight: 15,
+    greenAge: 59,
+    greenClass: "green",
+    greenName: "Howlling Winds Sr.",
+    greenSpecies: "dog",
+    greenVotes: 201,
+    greenWeight: 101,
+}
+
+*/
+
 
 
 
 // 7.
-var votes;
+var votes = dogs.reduce(function(count, currObj, array) {
+    count += currObj.votes;
+    return count;
+}, 0);
